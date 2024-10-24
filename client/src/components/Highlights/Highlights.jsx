420a53a32c560aeb7c428964cdd3de942e1460a6
Rebecca Burke <rebecca.burke@radancy.com>
2024-10-23 22:59:45 -0400

    more highlights

--- client/src/components/Highlights/Highlights.jsx
+++ client/src/components/Highlights/Highlights.jsx
@@ -7,11 +7,23 @@ import {
   booksByReadingLength,
   averageReading,
 } from "../../utils/helpers";
+import _ from 'lodash'
 
 export default function Highlights({ data }) {
   const topAuthors = booksByAuthor(data);
-  const shortestRead = booksByReadingLength(data).sort((a, b) => a.duration - b.duration)[0]
-  const longestRead = booksByReadingLength(data).sort((a, b) => b.duration - a.duration)[0]
+  const shortestRead = booksByReadingLength(data).sort(
+    (a, b) => a.duration - b.duration
+  )[0];
+  const longestRead = booksByReadingLength(data).sort(
+    (a, b) => b.duration - a.duration
+  )[0];
+  const shortestBook = [...data].sort((a, b) => a.pageCount - b.pageCount)[0];
+  const longestBook = [...data].sort((a, b) => b.pageCount - a.pageCount)[0];
+  const oldestBook = [...data].sort(
+    (a, b) => a.yearPublished - b.yearPublished
+  )[0];
+  const oneStars = [...data].filter((book) => book.rating === 1);
+  const fiveStars = _.uniqBy([...data].filter((book) => book.rating === 5), 'title');
   return (
     <Box>
       <Typography variant="h4" color={purple[800]}>
@@ -50,7 +62,9 @@ export default function Highlights({ data }) {
           Average Read Length:
         </Typography>
         <Stack>
-          <Typography color={purple[800]}>{`${averageReading(data)} days`}</Typography>
+          <Typography color={purple[800]}>{`${averageReading(
+            data
+          )} days`}</Typography>
         </Stack>
       </Box>
       <Box display="flex">
@@ -58,7 +72,12 @@ export default function Highlights({ data }) {
           Longest Read:
         </Typography>
         <Stack>
-          <Typography color={purple[800]}>{longestRead.title}<span style={{color: 'black'}}>{` - ${longestRead.duration} days`}</span></Typography>
+          <Typography color={purple[800]}>
+            {longestRead.title}
+            <span
+              style={{ color: "black" }}
+            >{` - ${longestRead.duration} days`}</span>
+          </Typography>
         </Stack>
       </Box>
       <Box display="flex">
@@ -66,7 +85,71 @@ export default function Highlights({ data }) {
           Shortest Read:
         </Typography>
         <Stack>
-          <Typography color={purple[800]}>{shortestRead.title}<span style={{color: 'black'}}>{` - ${shortestRead.duration} days`}</span></Typography>
+          <Typography color={purple[800]}>
+            {shortestRead.title}
+            <span
+              style={{ color: "black" }}
+            >{` - ${shortestRead.duration} days`}</span>
+          </Typography>
+        </Stack>
+      </Box>
+      <Box display="flex">
+        <Typography sx={{ fontWeight: "bold", width: 180 }}>
+          Longest Book:
+        </Typography>
+        <Stack>
+          <Typography color={purple[800]}>
+            {longestBook.title}
+            <span
+              style={{ color: "black" }}
+            >{` - ${longestBook.pageCount} pages`}</span>
+          </Typography>
+        </Stack>
+      </Box>
+      <Box display="flex">
+        <Typography sx={{ fontWeight: "bold", width: 180 }}>
+          Shortest Book:
+        </Typography>
+        <Stack>
+          <Typography color={purple[800]}>
+            {shortestBook.title}
+            <span
+              style={{ color: "black" }}
+            >{` - ${shortestBook.pageCount} pages`}</span>
+          </Typography>
+        </Stack>
+      </Box>
+      <Box display="flex">
+        <Typography sx={{ fontWeight: "bold", width: 180 }}>
+          Oldest Book:
+        </Typography>
+        <Stack>
+          <Typography color={purple[800]}>
+            {oldestBook.title}
+            <span
+              style={{ color: "black" }}
+            >{` - ${oldestBook.yearPublished}`}</span>
+          </Typography>
+        </Stack>
+      </Box>
+      <Box display="flex">
+        <Typography sx={{ fontWeight: "bold", width: 180 }}>
+          1 Star Reads:
+        </Typography>
+        <Stack>
+          {oneStars.map((book) => (
+            <Typography key={book.id}>{book.title}</Typography>
+          ))}
+        </Stack>
+      </Box>
+      <Box display="flex">
+        <Typography sx={{ fontWeight: "bold", width: 180 }}>
+          5 Star Reads:
+        </Typography>
+        <Stack>
+          {fiveStars.map((book) => (
+            <Typography key={book.id}>{book.title}</Typography>
+          ))}
         </Stack>
       </Box>
     </Box>
